98cff8786a901e6e8ec71670be5084ad9894ccf4
Ram-saddist <nagulavancharavisivaramteja@gmail.com>
2024-06-19 11:06:27 +0530

    navigation

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import Navigation from './Navigation/Navigation'
 import Signup from './Signup/Signup'
 import Login from './Login/Login'
-
+import Home from './Home/Home'
 export default function App() {
   return (
     <BrowserRouter>
       <Navigation/>
       <Routes>
-        <Route path="/"/>
+        <Route path="/" element={<Home/>}/>
         <Route path="/signup" element={<Signup/>}/>
         <Route path="/login" element={<Login/>}/>
       </Routes>
